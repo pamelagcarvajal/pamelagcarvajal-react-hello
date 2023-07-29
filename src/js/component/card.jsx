84d6b22e605d23c2a59d50adec34899c3f7d97c9
pamelagcarvajal <130812633+pamelagcarvajal@users.noreply.github.com>
2023-07-29 04:38:30 +0000

    landind page

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -6,7 +6,7 @@ const Card = () => {
         <img src="https://img.freepik.com/foto-gratis/fondo-gris-pintado_53876-94041.jpg" className="card-img-top" alt=" " />
           <div className="card-body">
             <h5 className="card-tittle">Card Tittle</h5>
-            <p className="Card Text">fdyftrhdyjtfdrddd</p>
+            <p className="Card Text">Card card card card card card</p>
             <a href="#" className="btn btn-primary">Go somewhere</a>
           </div>
       </div>
